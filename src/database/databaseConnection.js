82e2d58a1ce7de60d134f5ad5d0325b1042c9eac
Ahmed Salama <acmainshams@gmail.com>
2021-03-02 22:47:53 +0200

    hello

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose'); //primary
 //const validator = require('validator');
-const url = 'mongodb+srv://taskapp:Salamas#s$s123@cluster0.zuhkc.mongodb.net/TaskApp?retryWrites=true'//database name
+const url = 'mongodb+srv://taskapp:Salamas%23s$s123@cluster0.zuhkc.mongodb.net/TaskApp?retryWrites=true'//database name
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }); 
