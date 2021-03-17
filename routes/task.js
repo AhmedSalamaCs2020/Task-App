c1aba6ed6a2e8545da785e5dae6028c8c2c78603
Ahmed Salama <acmainshams@gmail.com>
2021-03-17 03:24:50 +0200

    message

--- routes/task.js
+++ routes/task.js
@@ -27,7 +27,7 @@ router.get("/tasks",auth,async(req,res)=>{
      }
      console.log(match);
    try {
-  const tasks  = await taskModel.find(match).limit(req.query.limit)
+  const tasks  = await taskModel.find(match).limit(parseInt(req.query.limit))
   res.status(200).send(tasks) 
    } catch (error) {
       res.status(500).send(error)
