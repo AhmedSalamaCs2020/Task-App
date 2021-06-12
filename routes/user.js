31a2acfbc4619c640f5589ee530a726fbb3c280e
Ahmed Salama <acmainshams@gmail.com>
2021-06-12 15:40:54 +0200

    issues

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ res.send({"message":"false"})
 router.get('/getUsers', auth,async(req, res) => {
 //done 
 try {
-  const data =await userModel.find({"type":"captain"})
+  const data =await userModel.find({"type":req.body["type"]})
   res.status(200).send(data)
 } catch (error) {
   res.status(400).send(error)
