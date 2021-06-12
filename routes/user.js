bf6270167f659b17ec4c847a6eb56b208ca80214
Ahmed Salama <acmainshams@gmail.com>
2021-06-12 13:56:48 +0200

    reviews

--- routes/user.js
+++ routes/user.js
@@ -44,8 +44,15 @@ res.send(req.user)
 })
 //
 router.patch("/users",auth,async (req,res)=>{
-  const data =await userModel.findByIdAndUpdate(req.body["id"],{activated: req.body["activated"]})
-  res.send(data)
+  const data =await userModel.findByIdAndUpdate(req.body["id"],{activated: req.body["activated"]},    function (err, docs) {
+    if (err){
+      res.send(err)
+    }
+    else{
+      res.send(data)
+    }
+})
+
 })
 //
 // delete using token also
